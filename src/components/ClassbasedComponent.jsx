56dd3fd2134669c78fc0d4744dbeca60abe23954
ashwinrawate <rawateashwin@gmail.com>
2025-09-26 22:39:13 +0530

    State management

--- src/components/ClassbasedComponent.jsx
+++ src/components/ClassbasedComponent.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 
 class ClassBasedComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+      count1: 0,
+    };
+  }
   render() {
+    let { count, count1 } = this.state;
     return (
       <div>
-        <h1>Class Based Component {this.props.address}</h1>
+        <h1>
+          {/* Update the state variable */}
+          <button
+            onClick={() => {
+              this.setState({ count: this.state.count + 1 });
+            }}
+          >
+            Inc count
+          </button>
+          Class Based Component {this.props.address} and count = {count} and
+          next count is : {count1}
+        </h1>
       </div>
     );
   }
